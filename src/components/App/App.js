68c64b527dd3853d2b6cca668b91a78d6013c468
Daniil Pesotskyi <pesotckiydanya@gmail.com>
2023-05-23 01:20:41 +0300

    рефакторинг на хуки

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,45 +1,58 @@
-// import css from './App.module.css'
-import {React, Component } from 'react'
+import { React, useState } from 'react';
 
 import Section from '../Section/Section';
 import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
 import Statistics from '../Statistics/Statistics';
 import Notification from '../Notification/Notification';
 
-class App extends Component {
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad
-  }
+  const countPositiveFeedbackPercentage = () => {
+    return (good / (good + neutral + bad)) * 100;
+  };
 
-  countPositiveFeedbackPercentage() {
-    return (this.state.good / (this.state.good + this.state.neutral + this.state.bad)) * 100
-  }
+  const onLeaveFeedback = option => {
+    switch (option) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(good + 1);
+        break;
+      case 'bad':
+        setBad(good + 1);
+        break;
+      default:
+        return;
+    }
+  };
 
-  onLeaveFeedback = (option) => {
-    this.setState(prevState => ({
-      [option]: prevState[option] + 1
-    }))
-  }
-
-  render() {
-    return (
-      <>
-        <Section title={'Feedback'}>
-          <FeedbackOptions options={Object.keys(this.state)} onFeedback={this.onLeaveFeedback}/>
-          <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}>
-            <Notification message="There is no feedback" />
-          </Statistics>
-        </Section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section title={'Feedback'}>
+        <FeedbackOptions
+          options={["good", "neutral", "bad"]}
+          onFeedback={onLeaveFeedback}
+        />
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        >
+          <Notification message="There is no feedback" />
+        </Statistics>
+      </Section>
+    </>
+  );
+};
 
 export default App;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,28 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import css from './Statistics.module.css';
 import PropTypes from 'prop-types';
 
-class Statistics extends Component {
-    render() {
-        return (
-            <>
-                <h2 className={css.title}>Statistics</h2>
-                    {this.props.total === 0 ? (<>{this.props.children}</>) : (
-                        <>
-                            <div className={css.statWrap}>
-                                <ul className={css.statList}>
-                                    <li className={css.statItem}>Good: {this.props.good}</li>
-                                    <li className={css.statItem}>Neutral: {this.props.neutral}</li>
-                                    <li className={css.statItem}>Bad: {this.props.bad}</li>
-                                </ul>
-                            </div>
-                            <p className={css.addData}>Total: {this.props.total}</p>
-                            <p className={css.addData}>Positive feedback: {isNaN(this.props.positivePercentage) ? 0 : Math.round(this.props.positivePercentage)}%</p>
-                        </>
-                    )}
-            </>
-        )
-    }
+const Statistics = ({good, neutral, bad, total, positivePercentage, children}) => {
+    return (
+        <>
+            <h2 className={css.title}>Statistics</h2>
+                {total === 0 ? (<>{children}</>) : (
+                    <>
+                        <div className={css.statWrap}>
+                            <ul className={css.statList}>
+                                <li className={css.statItem}>Good: {good}</li>
+                                <li className={css.statItem}>Neutral: {neutral}</li>
+                                <li className={css.statItem}>Bad: {bad}</li>
+                            </ul>
+                        </div>
+                        <p className={css.addData}>Total: {total}</p>
+                        <p className={css.addData}>Positive feedback: {isNaN(positivePercentage) ? 0 : Math.round(this.props.positivePercentage)}%</p>
+                    </>
+                )}
+        </>
+    )
 }
 
 Statistics.propTypes = {
